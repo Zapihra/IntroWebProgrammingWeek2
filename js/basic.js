14121a276a636bfb9b360e8acf4c852d3817fe9c
Zapihra <iida.vaaja@student.lut.fi>
2023-06-07 16:50:01 +0300

    add data to table

---
+++ js/basic.js
@@ -0,0 +1,32 @@
+const buttonAdd = document.getElementById("submit-data");
+
+buttonAdd.addEventListener("click", () => {
+    const username = document.getElementById("input-username").value;
+    const email = document.getElementById("input-email").value;
+    const address = document.getElementById("input-address").value;
+    const admin = document.getElementById("input-admin").checked;
+
+    const tableRow = document.createElement("tr");
+    const tableData1 = document.createElement("td");
+    const tableData2 = document.createElement("td");
+    const tableData3 = document.createElement("td");
+    const tableData4 = document.createElement("td");
+
+    tableData1.appendChild(document.createTextNode(username));
+    tableData2.appendChild(document.createTextNode(email));
+    tableData3.appendChild(document.createTextNode(address));
+    if (admin == true) {
+        tableData4.appendChild(document.createTextNode("X"));
+    }
+    else {
+        tableData4.appendChild(document.createTextNode("-"));
+    }
+    
+    tableRow.appendChild(tableData1);
+    tableRow.appendChild(tableData2);
+    tableRow.appendChild(tableData3);
+    tableRow.appendChild(tableData4);
+
+    document.getElementById("table").appendChild(tableRow);
+
+})
